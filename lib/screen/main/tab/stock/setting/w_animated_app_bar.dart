786ab15ff235c0137764c2681ff27d1dd998a192
itbebop <it.bebop@gmail.com>
2023-11-06 22:56:55 +0900

    2023/11/06/ 중간값 있는 애니메이션

--- lib/screen/main/tab/stock/setting/w_animated_app_bar.dart
+++ lib/screen/main/tab/stock/setting/w_animated_app_bar.dart
@@ -17,7 +17,7 @@ class AnimatedAppBar extends StatefulWidget {
 
 class _AnimatedAppBarState extends State<AnimatedAppBar> {
   //final duration = 250.ms; // 적용이 안돼서 아래로
-  Duration duration = 100.ms; // 이렇게 초기화하고 아래 build함수에서 변경
+  Duration get duration => 10.ms; // 이렇게 초기화하고 아래 build함수에서 변경
   double scrollPosition = 0;
 
   @override
@@ -32,9 +32,19 @@ class _AnimatedAppBarState extends State<AnimatedAppBar> {
 
   bool get isTriggered => scrollPosition > 80;
   bool get isNotTriggerd => !isTriggered;
+
+  // isTriggered의 양쪽 중간값 있는 애니메이션 만들기 위한 함수
+  double getValue(double initial, double target) {
+    if (isTriggered) {
+      return target;
+    }
+    double fraction = scrollPosition / 80;
+    return initial + (target - initial) * fraction;
+  }
+
   @override
   Widget build(BuildContext context) {
-    duration = 250.ms;
+    //duration = 250.ms;
     return Container(
       width: double.infinity,
       color: context.backgroundColor,
@@ -49,7 +59,7 @@ class _AnimatedAppBarState extends State<AnimatedAppBar> {
             ).p20(),
             AnimatedContainer(
               duration: duration,
-              padding: EdgeInsets.only(left: isNotTriggerd ? 20 : 50, top: isNotTriggerd ? 50 : 15),
+              padding: EdgeInsets.only(left: getValue(20, 50), top: getValue(50, 15)),
               child: AnimatedDefaultTextStyle(
                 style: TextStyle(fontSize: isNotTriggerd ? 30 : 18, fontWeight: FontWeight.bold),
                 duration: duration,
